85b7cf215666ea987bf0012f077033a9966f0491
Justin Wright <31335870+Dkmariolink@users.noreply.github.com>
2019-11-22 22:28:11 -0500

    Added functioning stats page with counters.
    Not fully implemented, resume button does not work.

--- main/java/com/example/thekingsandqueensgame/Game.java
+++ main/java/com/example/thekingsandqueensgame/Game.java
@@ -1,5 +1,6 @@
 package com.example.thekingsandqueensgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,10 +8,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game extends AppCompatActivity {
+    private Button resume;
     private Button c1;
     private int Strength = 0;
     private int Intelligence = 0;
@@ -18,8 +26,11 @@ public class Game extends AppCompatActivity {
     private int Charm = 0;
     private int Luck = 0;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page1);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -47,15 +58,39 @@ public class Game extends AppCompatActivity {
         inflater.inflate(R.menu.main_menu,menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.item1:
                 // stats
-                setContentView(R.layout.stats);
+                setContentView(R.layout.activity_stats);
                 Toolbar toolbar = findViewById(R.id.toolbar);
                 setSupportActionBar(toolbar);
+                final TextView str = findViewById(R.id.strength);
+                str.setText(Integer.toString(Strength));
+                final TextView intel = findViewById(R.id.intelligence);
+                intel.setText(Integer.toString(Intelligence));
+                final TextView agi = findViewById(R.id.agility);
+                agi.setText(Integer.toString(Agility));
+                final TextView cha = findViewById(R.id.charm);
+                cha.setText(Integer.toString(Charm));
+                final TextView luc = findViewById(R.id.luck);
+                luc.setText(Integer.toString(Luck));
+                resume = findViewById(R.id.resume);
+                resume.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View resume) {
+                        switch (resume.getId()) {
+                            case R.id.resume:
+                                ((ViewGroup) resume.getParent()).removeView(resume);
+                                Toolbar toolbar = findViewById(R.id.toolbar);
+                                setSupportActionBar(toolbar);
+                                break;
+                        }
+                    }
+
+
+                });
                 break;
             case R.id.item2:
                 // Save
@@ -73,7 +108,10 @@ public class Game extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return true;
+
     }
-}
+
+    }
+
 
 
